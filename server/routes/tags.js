a01d89d0f838033562deef02b0112452034e8c1c
mik <beneschan@gmail.com>
2026-06-09 13:44:46 -0700

    search results can now be filtered by tags

--- server/routes/tags.js
+++ server/routes/tags.js
@@ -18,19 +18,88 @@ tagsRouter.get('/', async (req, res) => {
 });
 
 tagsRouter.post('/search', async (req,res) => {
-    let query = req.body.prompt
-    let result = await Post.aggregate([
-        {
-            $search: {
-                "index": "searchv1",
-                "text": {
-                    "query": query,
-                    "path": ["title","subtitle","content","tags","recipeCards"]
-                }
-            }
+    let result = []
+    const query = (req.body.prompt || '').trim()
+    const tags = req.body.tags
+    const synonymName = 'syn'
+    const stopWords = new Set([
+        'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for',
+        'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of', 'on', 'or',
+        'such', 'that', 'the', 'their', 'then', 'there', 'these', 'they',
+        'this', 'to', 'was', 'will', 'with'
+    ])
+
+    if (!query) {
+        return res.json({ html: '', count: 0 })
+    }
+
+    // check that query isn't just English stopwords
+    const meaningfulTokens = query
+        .toLowerCase()
+        .split(/[^a-z0-9]+/)
+        .filter((token) => token && !stopWords.has(token))
+
+    if (meaningfulTokens.length === 0) {
+        result = []
+    } else {
+        try {
+            result = await Post.aggregate([
+                {
+                    $search: {
+                        "index": "searchv1",
+                        "compound": {
+                            "should": [
+                                {
+                                    "autocomplete": {
+                                        "query": query,
+                                        "path": "title",
+                                    }
+                                },
+                                {
+                                    "text": {
+                                        "query": query,
+                                        "path": "subtitle",
+                                        "synonyms": synonymName
+                                    }
+                                },
+                                {
+                                    "text": {
+                                        "query": query,
+                                        "path": "content",
+                                        "synonyms": synonymName
+                                    }
+                                },
+                                {
+                                    "text": {
+                                        "query": query,
+                                        "path": "tags",
+                                        "synonyms": synonymName
+                                    }
+                                },
+                                {
+                                    "text": {
+                                        "query": query,
+                                        "path": [
+                                            "recipeCards.label",
+                                            "recipeCards.procedure",
+                                            "recipeCards.ingredients.sectionName",
+                                            "recipeCards.ingredients.ings"
+                                        ],
+                                        "synonyms": synonymName
+                                    }
+                                }
+                            ],
+                            "minimumShouldMatch": 1
+                        }
+                    }
+                },
+                ...(tags.length > 0 ? [{ $match: { tags: { $all: tags } } }] : [])
+            ])
+        } catch (error) {
+            throw error
         }
-    ]
-    )
+    }
+
     res.render(
         'partials/foundPosts',
         { 
@@ -48,6 +117,7 @@ tagsRouter.post('/search', async (req,res) => {
 tagsRouter.post('/tagFind', async (req,res) => {
     let query = req.body.prompt
     let result = await Post.find({tags: {$all: query}}).sort({publishDate: 'desc'})
+
     res.render(
         'partials/foundPosts',
         { 
